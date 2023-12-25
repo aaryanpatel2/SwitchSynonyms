757a18632b7059728fddea791c6e58c528ee581e
Aaryan <aaryanp802@gmail.com>
2023-12-24 22:18:52 -0600

    second window and finds most used word

--- main.py
+++ main.py
@@ -13,13 +13,16 @@ greeting.pack()
 greeting_answer = Text(window, height=5, width=25, bg="black")
 greeting_answer.pack()
 
-
+Button(window, text="Welcome/Quit", command=lambda: welcome_greeting()).pack(pady=20)
 
 def welcome_greeting():
     global greeting
     if greeting_answer.get("1.0", "end-1c") == "c" or greeting_answer.get("1.0", "end-1c") == "C":
         Button(window, text="File Creation", command=lambda: create_file()).pack(pady=20)
-        Label(text="Enter your file below (Click File Creation").pack()
+        Label(text="Enter your file below (Click File Creation to confirm selection)").pack()
+        global file_enter
+        file_enter = Text(window, height=25, width=50, bg="black",)
+        new_window_button = Button(window, text="Open Synonym Window", command=lambda: open_new_window()).pack(side=BOTTOM, padx=20, pady=20)
     elif greeting_answer.get("1.0", "end-1c") == "q" or greeting_answer.get("1.0", "end-1c") == "Q":
         quit()
     else:
@@ -29,26 +32,61 @@ def welcome_greeting():
 
 #Creates a user dialog box where users can type in their document and
 # it will create a text file with the contents in the dialog box
+#file_enter = Text(window, height=25, width=50, bg="black", pady=20)
+
 def create_file():
-    file_create = open("synonymswitch.txt", 'w')
-    file_enter_string = ""
-    file_enter = Text(window, height=25, width=25, bg="black")
     file_enter.pack()
-
-    #TODO: "fix command or move button" Button(window,text="Submit File", command=lambda: create_file()).pack(pady=20)
-
-    file_enter = file_enter_string
+    global file_create
+    file_create = open("synonymswitch.txt", 'w')
+    file_enter_string = file_enter.get("1.0", "end-1c")
     file_create.write(file_enter_string)
+    file_create.close()
 
 #Parses the text file and finds the most used word
 # (add to list and increment count)
-#def find_syn():
+def find_syn():
+    file_create = open("synonymswitch.txt", 'r')
+    words = file_create.read()
+    #print(words)
+    words_list = words.split(" ")
+    #print(words_list)
+    word_frame = Frame(syn_window).pack(pady=20)
 
-    #TODO: create a user dialog box where user can type in their document and it will create a text file off that
+    for word in words_list:
+        Label(syn_window, word_frame, text = "● " + word).pack()
+
+    syn_list = []
+
+    danger_words = ["the", "a", "an"]
+
+    for word in list(words_list):
+        if word in danger_words:
+            words_list.remove(word)
+
+    print(words_list)
+
+    for i in range(len(words_list)):
+        if words_list[i] == words_list[i-1]:
+            syn_list.append(words_list[i])
+
+    print(syn_list)
+
+    Label(syn_window, text = "Here are your most used words").pack(pady=40)
+    syn_frame = Frame(syn_window).pack(pady=20)
+    for synonym in syn_list:
+        Label(syn_window, syn_frame, text = "● " + synonym).pack()
 
 
 
-Button(window, text="Welcome/Quit", command=lambda: welcome_greeting()).pack(pady=20)
+def open_new_window():
+    global syn_window
+    syn_window = Toplevel(window)
+    syn_window.title("SynonymSwitch")
+    syn_window.geometry("300x300")
+    Button(syn_window, text="Find Synonym", command=lambda: find_syn()).pack(pady=20)
 
 
-window.mainloop()
+mainloop()
+
+
+
