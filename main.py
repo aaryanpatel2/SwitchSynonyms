8c10c61f7cb816773f66b9c088caa3aeeaf254b1
Aaryan <aaryanp802@gmail.com>
2023-12-31 16:04:51 -0600

    added functionality to choose synonym and change in original text

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def welcome_greeting():
         Button(window, text="File Creation", command=lambda: create_file()).pack(pady=20)
         Label(text="Enter your file below (Click File Creation Again When Done)").pack()
         global file_enter
-        file_enter = Text(window, height=25, width=100, bg="black",)
+        file_enter = Text(window, height=25, width=100, wrap=WORD, bg="black",)
         new_window_button = Button(window, text="Open Synonym Window", command=lambda: open_new_window()).pack(side=BOTTOM, padx=20, pady=20)
     elif greeting_answer.get("1.0", "end-1c") == "q" or greeting_answer.get("1.0", "end-1c") == "Q":
         quit()
@@ -48,10 +48,11 @@ def create_file():
 #Parses the text file and finds the most used word
 # (add to list and increment count)
 def find_syn():
+    global words
+    global words_list
     file_create = open("synonymswitch.txt", 'r')
     words = file_create.read()
     words_list = words.split(" ")
-    words = words.replace("!", "")
     words_list = [words.lower() for words in words_list]
 
 
@@ -245,7 +246,7 @@ def find_syn():
     Label(syn_window, syn_frame, text = "• " + most_used_word).pack()
     Label(syn_window, syn_frame, text="").pack()
     thesaurus_button = Button(syn_window, text= "Open Thesaurus.com", command=lambda: open_thesaurus())
-    thesaurus_button.place(x = 670, y = 600)
+    thesaurus_button.place(x = 670, y = 730)
 
 
 
@@ -284,6 +285,30 @@ def webscrape():
 
     for syn in new_list:
         list_display.insert(END, syn + "\n")
+
+    global syn_replace_text
+    Label(syn_window, text="Choose a synonym to replace and type below").pack()
+    syn_replace_text = Text(syn_window, height=5, width=10, bg="black")
+    syn_replace_text.pack()
+    Button(syn_window, text="Submit Change", command=lambda: replace()).pack(pady=20)
+def replace():
+    syn_replace = syn_replace_text.get("1.0", "end-1c")
+    if syn_replace == "":
+        Label(syn_window, text="No synonym chosen, text remains same")
+    else:
+        replace_window = Toplevel(syn_window)
+        replace_window.title("SynonymSwitch")
+        replace_window.geometry("300x300")
+
+        Label(replace_window, text="New Text with Synonyms").pack()
+
+        replace_display = Text(replace_window, height=25, width=50, wrap=WORD, bg="black")
+        replace_display.pack(pady=10)
+
+        new_list = [word if word != most_used_word else syn_replace for word in words_list]
+
+        for word in new_list:
+            replace_display.insert(END, word + " ")
 
 
 mainloop()
